b1a0cbcbaeb35bd75e7efc88383f978bcfd080f9
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-06-23 09:23:54 -0700

    good pass age calculator test one

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -3,7 +3,7 @@ import AgeCalculator from '../src/age-calculator.js';
 describe('AgeCalculator', () => {
 
   test('should correctly determine the age of a person based on the location of a planet', () => {
-    const ageCalculator = new AgeCalculator();
+    const ageCalculator = new AgeCalculator(40);
     expect(ageCalculator.earthAge).toEqual(40);
   });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,3 +1,9 @@
 export default class AgeCalculator {
-
+  constructor(currentAge) {
+    this.earthAge = currentAge;
+    this.mercuryYears = 0;
+    this.venusYears = 0;
+    this.marsYears = 0;
+    this.jupiterYears = 0;
+  }
 }
