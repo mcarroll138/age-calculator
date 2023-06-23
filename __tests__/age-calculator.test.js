f7b1f035a3e4bece6f4abb548a12c7231e3c21a8
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-06-23 09:33:39 -0700

    good fail age calculator test mercury years

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -2,8 +2,13 @@ import AgeCalculator from '../src/age-calculator.js';
 
 describe('AgeCalculator', () => {
 
-  test('should correctly determine the age of a person based on the location of a planet', () => {
+  test('should correctly determine the earth age of a person based on their input', () => {
     const ageCalculator = new AgeCalculator(40);
     expect(ageCalculator.earthAge).toEqual(40);
   });
+  test('should correctly determine the age of a person in mercury years', () => {
+    const ageCalculator = new AgeCalculator(40);
+    expect(ageCalculator.mercuryYears).toEqual(40 / .24);
+  });
+
 });
